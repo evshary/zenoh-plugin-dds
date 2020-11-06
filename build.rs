f579dccd6c04b57471c84e9efae86c3d1801ed66
ChenYing Kuo <evshary@gmail.com>
2020-11-06 11:57:05 +0800

    Add more library dependency path.

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() {
     println!("cargo:rustc-link-search=/usr/local/lib");
+    println!("cargo:rustc-link-search=/home/ros/workspace/zenoh_dds_dep_ws/install/lib");
+    println!("cargo:rustc-link-search=/home/evshary/workspace/zenoh_dds_dep_ws/install/lib");
     println!("cargo:rustc-link-lib=cdds-util");
 }
